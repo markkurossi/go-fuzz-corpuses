03cdfae34c05b6efc9e708676e989c7a18b4f76f
Markku Rossi <mtr@iki.fi>
2021-01-24 11:16:08 +0200

    Bristol circuit tests.

---
+++ mpc/bristol/fuzz.go
@@ -0,0 +1,24 @@
+//
+// Copyright (c) 2021 Markku Rossi
+//
+// All rights reserved.
+//
+
+package mpcl
+
+import (
+	"bytes"
+
+	"github.com/markkurossi/mpc/circuit"
+)
+
+func Fuzz(data []byte) int {
+	circ, err := circuit.ParseBristol(bytes.NewReader(data))
+	if err != nil {
+		if circ != nil {
+			panic("circ != nil on error")
+		}
+		return 0
+	}
+	return 1
+}
